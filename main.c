72ee71802eca04e5c9be50439d9d10bad7e28e14
Viacheslav <v.konovalov@crystals.ru>
2022-12-16 17:05:26 +0300

    Init

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * @brief
+ *
+ * @return int
+ */
+int main()
+{
+    client_phone_correction_detection_test();
+    client_phone_correction_test();
+    in_fw_test_scenario();
+    return 0;
+}
